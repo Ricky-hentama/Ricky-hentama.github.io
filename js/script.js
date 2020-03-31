78cbcc7b221281b2021fc415305692212d64d878
Ricky Hentama <rickyhendrapratama4@gmail.com>
2020-04-01 06:17:49 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 $("#tombol-lanjut").click(function() {
   let nama = $("#nama").val();
   console.log(nama);
+  if (nama == "") {
+    console.log("WAJIB TULIS NAMA!");
+  } else {
+    $("#phase-1").addClass("hide");
+
+    $("#phase-2").removeClass("hide");
+    $("#nama-user").html(nama);
+  }
 });
