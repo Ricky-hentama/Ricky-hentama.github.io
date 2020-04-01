013a29a2936e4f436b7c81bb24bd9b9698b97c76
Ricky Hentama <rickyhendrapratama4@gmail.com>
2020-04-01 21:54:08 +0700

    update phase 3

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
+  // phase-1
   $("#tombol-lanjut").click(function() {
     let namaUser = $("#nama").val();
-    $("#phase-1").addClass("hide");
-    $("#phase-2").removeClass("hide");
-    $("#nama-user").html(namaUser);
+    if (namaUser != "") {
+      $("#phase-1").addClass("hide");
+      $("#phase-2").removeClass("hide");
+      $(".nama-user").html(namaUser);
+
+      // phase-2
+      $("#phase-2 button").click(function(e) {
+        let jwb = e.target.dataset.jawab;
+        if (jwb == "ora") {
+          $("#phase-2").addClass("hide");
+          $("#phase-3").removeClass("hide");
+          $(".nama-user").html(namaUser);
+        }
+      });
+    }
   });
 });
