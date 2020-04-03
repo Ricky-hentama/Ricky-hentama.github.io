09d033d5085c4a425d546225d10254d4b137a5e2
Ricky Hentama <rickyhendrapratama4@gmail.com>
2020-04-03 22:39:15 +0700

    update

---
+++ js/kategori.js
@@ -0,0 +1,37 @@
+// kategori
+let k_toi = [
+  ["naksir adik kelas"],
+  ["naksir kakel"],
+  ["forto pas jam kosong"],
+  ["naksir guru"],
+  ["manggil cewek dari lantai 4"],
+  ["pulang sekolah nggopal"],
+  ["beli batagor depan sekolah"],
+  ["main ludo"],
+  ["rebutan cireng"],
+  ["pulang sekolah main voli"],
+  ["wifian ruang listrik"],
+  ["diomeli pak kuseno"],
+  ["diomeli pak iron man"],
+  ["tidur waktu pelajaran"],
+  ["(kerja) (kelompol)"],
+  ["telat bayar kas"]
+];
+let k_mesum = [
+  ["adit 2"],
+  ["ade 3"],
+  ["apri 4"],
+  ["agung 5"],
+  ["alfat 6"],
+  ["andre 7"],
+  ["pandu 8"],
+  ["arfan 9"],
+  ["dena 10"],
+  ["elvin 11"],
+  ["faizal 12"],
+  ["fuad 13"],
+  ["gani 14"],
+  ["galih"],
+  ["hamdan"],
+  ["habibi"]
+];
--- js/script.js
+++ js/script.js
@@ -1,43 +1,4 @@
 $(document).ready(function() {
-  // kategori
-  let k_toi = [
-    ["adit"],
-    ["ade"],
-    ["apri"],
-    ["agung"],
-    ["alfat"],
-    ["andre"],
-    ["pandu"],
-    ["arfan"],
-    ["dena"],
-    ["elvin"],
-    ["faizal"],
-    ["fuad"],
-    ["gani"],
-    ["galih"],
-    ["hamdan"],
-    ["habibi"]
-  ];
-  let k_mesum = [
-    ["adit 2"],
-    ["ade 3"],
-    ["apri 4"],
-    ["agung 5"],
-    ["alfat 6"],
-    ["andre 7"],
-    ["pandu 8"],
-    ["arfan 9"],
-    ["dena 10"],
-    ["elvin 11"],
-    ["faizal 12"],
-    ["fuad 13"],
-    ["gani 14"],
-    ["galih"],
-    ["hamdan"],
-    ["habibi"]
-  ];
-
-  // Akhir kategori
   // phase-1
   $("#tombol-lanjut").click(function() {
     let namaUser = $("#nama").val();
@@ -63,19 +24,19 @@ $(document).ready(function() {
             $("#phase-3-iya").addClass("hide");
             $("#phase-4").removeClass("hide");
             $("#kategori-terpilih").html(textKategori);
-            let kartu = document.getElementsByClassName("kartu");
+            let isi = document.getElementsByClassName("isi");
 
             // berdasarkan kategori
             if (kategori == "toi") {
-              for (let i = 0; i < kartu.length; i++) {
+              for (let i = 0; i < isi.length; i++) {
                 k_toi.forEach((e, i) => {
-                  kartu[i].innerHTML = e;
+                  isi[i].innerHTML = e;
                 });
               }
             } else if (kategori == "mesum") {
-              for (let i = 0; i < kartu.length; i++) {
+              for (let i = 0; i < isi.length; i++) {
                 k_mesum.forEach((e, i) => {
-                  kartu[i].innerHTML = e;
+                  isi[i].innerHTML = e;
                 });
               }
             }
